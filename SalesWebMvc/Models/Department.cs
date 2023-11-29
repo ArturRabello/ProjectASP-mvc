f11aee79840b8648a645c6605d94043bddd0512d
Artur Rabello <artur_rabello@yahoo.com.br>
2023-11-29 14:47:22 -0300

    Model-Controller-View - Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
